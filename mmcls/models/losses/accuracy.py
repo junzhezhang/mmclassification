69dba9cbc4aceb3091c5397be70258ab26dcf265
junzhezhang <30921847+junzhezhang@users.noreply.github.com>
2021-06-22 23:25:16 +0800

    test - print

--- mmcls/models/losses/accuracy.py
+++ mmcls/models/losses/accuracy.py
@@ -32,6 +32,7 @@ def accuracy(pred, target, topk=1):
 
     res = []
     for k in topk:
+        print('k-topk',k,topk)
         correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
         res.append(correct_k.mul_(100.0 / pred.size(0)))
     return res[0] if return_single else res
